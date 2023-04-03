93b12643bfeb99c073cc4686dae5a8fefddb250b
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-04-03 15:26:12 -0300

    exerc adicionando chaves

---
+++ Fundamentos/secao3-JS/dia4-objetos/novasChaves.js
@@ -0,0 +1,17 @@
+student = {
+
+}
+
+function addProperty(object, key, value) {
+    object[key] = value;
+};
+let pizzas = {
+
+}
+addProperty(student, 'nome', 'Raí')
+addProperty(student, 'e-mail', 'rairodrigues_sf@hotmail.com')
+addProperty(student, 'telefone', '(22)999940592')
+addProperty(pizzas, 'sabor', 'catupiry')
+
+console.log(student);
+console.log(pizzas)
