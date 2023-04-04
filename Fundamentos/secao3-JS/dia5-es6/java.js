177cce6220acd621e6c8f6ff939dccdcb7e4ad96
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-04-04 15:00:45 -0300

    vou criar outra branch

---
+++ Fundamentos/secao3-JS/dia5-es6/java.js
@@ -0,0 +1,9 @@
+// Com o template literals
+console.log(`Primeira linha;
+Segunda linha;
+Terceira linha;`
+);
+
+// Sem o template literals:
+console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n');
+
