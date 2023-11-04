a57c82e9c57e77bf112df7a4042085252693790f
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-11-04 12:39:22 -0300

    fix import fs e cria endpoint de movies

--- back-end/secao4-dia2/exercicio-do-dia/src/app.js
+++ back-end/secao4-dia2/exercicio-do-dia/src/app.js
@@ -1,5 +1,30 @@
 const express = require('express');
 
 const app = express();
+const fs = require('fs').promises;
+const path = require('path');
+
+const moviesPath = path.resolve(__dirname, './movies.json');
+
+const readFile = async () => {
+  try {
+    const data = await fs.readFile(moviesPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Arquivo não pôde ser lido: ${error}`);
+  }
+};
+
+app.get('/movies/:id', async (req, res) => {
+  try {
+     const { id } = req.params;
+    const movies = await readFile();
+    console.log(movies);
+    const searchMovies = movies.find((movie) => movie.id === Number(id));
+    res.status(200).json(searchMovies);
+   } catch (err) {
+     res.status(500).send({ message: err.message });
+   }
+ });
 
 module.exports = app;
