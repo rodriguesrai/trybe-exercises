f0605a6db47f141627473c0e88d94968e640d75b
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-02-21 14:49:36 -0300

    exercício do dia

---
+++ cc/secao1-dia1/exercicio1.py
@@ -0,0 +1,5 @@
+def biggerNumber(a, b):
+    if a > b:
+        return a
+    else:
+        return b
---
+++ cc/secao1-dia1/exercicio3.py
@@ -0,0 +1,6 @@
+def parede_asteriscos(n):
+    if n > 0:
+        for i in range(n):
+            print('*' * n)
+
+parede_asteriscos(5)
