4435a94cd3186050bcaeb4606f1022d819dadfdf
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-04-02 17:07:29 -0300

    exercício do dia: getters, setters, atributos e modificadores de acesso

--- java/dia2.1/exercicio/src/main/java/com/betrybe/cars/ApplicationCars.java
+++ java/dia2.1/exercicio/src/main/java/com/betrybe/cars/ApplicationCars.java
@@ -3,6 +3,12 @@ package com.betrybe.cars;
 public class ApplicationCars {
 
   public static void main(String[] args) {
+    Carro carro = new Carro("Chevrolet", "Onix", 2021);
+    Carro carro1 = new Carro("Fiat", "Uno", 2020);
+    Carro carro2 = new Carro("Ford", "Ka", 2019);
 
+    System.out.println(Carro.quantidadeDeCarros());
+
+    System.out.println(carro.exibirInformacoes());
   }
 }
